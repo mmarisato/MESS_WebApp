8e54abcb717158318dd1b731ce453f56902667de
Mari Sato <msato@smu.edu>
2026-02-22 17:52:30 -0600

    Homepage

--- src/app.mjs
+++ src/app.mjs
@@ -1,13 +1,21 @@
 import express, { response } from 'express'; 
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express(); 
 
 const port = 3333; 
 
+//Chat
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//Chat
+app.use(express.static(path.join(__dirname,"..", "public")));
+
 app.get('/', (request, response) => {
-    response.send("Hello, World!"); 
+    response.send(path.join(__dirname, "..","public","index.html")); 
 });
 
 app.listen(port,() =>{
-    console.log(`Application listening at port ${port}`); 
+    console.log(`Listening at http://localhost:${port}`); 
 }); 
